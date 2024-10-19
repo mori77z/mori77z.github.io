95ccc9e618b2ae460294bccdb27ef4b3f8ab8d60
mori77z <moritzggauss@gmail.com>
2024-10-19 19:17:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ messageButton.addEventListener('click', function () {
     contactForm.classList.toggle('active');
     messageButton.classList.toggle('active');
 });
+type="text/javascript">
+  emailjs.init('sGAgJH_6DUGxUUIxj'); // EmailJS Benutzer-ID
+
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    emailjs.sendForm('service_141csud', 'template_cn0ssgw', this)
+      .then(function() {
+        alert('Message sent successfully!');
+      }, function(error) {
+        alert('Failed to send message. Please try again.');
+      });
+  });
