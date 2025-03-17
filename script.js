b226fae89318320684fd6f9b88e8756f60105100
mori77z <moritzggauss@gmail.com>
2025-03-17 23:55:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let isFlipping = false;
 
     function randomChar() {
-        const symbols = "✪✹❦♬♪♩★❥✱♫♠♞♥";
+        const symbols = "✪✹❦☭★❥✱♫♠♞♥";
         return symbols[Math.floor(Math.random() * symbols.length)];
     }
 
@@ -43,7 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
             ticking = true;
         }
     });
-});
 
     const images = document.querySelectorAll(".img-container img");
     if (images.length === 0) {
